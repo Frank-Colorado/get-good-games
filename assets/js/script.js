84798129ec8471f2773536a21abfd050e5e23c4b
Omar <Omarggm@gmail.com>
2023-03-18 21:09:23 -0700

    added script to respond to inputs via search bar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,4 @@
-
-fetch('https://api.rawg.io/api/platforms?key=a29e01702f3d4aa2a58af885563c92b7')
-
-.then(response => response.json())
-
-.then(data => {
-
-console.log(data);
-
-document.getElementById('tester').textContent = data.results[0].name;
-	   
-	  
- })
-
-
-
+/*
 var requestOptions = {
 
 	method: 'GET',
@@ -29,3 +14,29 @@ var requestOptions = {
 	  .then(result => console.log(result))
 
 	  .catch(error => console.log('error', error));
+
+*/
+
+//get the search input element by its ID
+const searchInput = document.getElementById('search_input');
+
+
+//add an event listener to search input field
+searchInput.addEventListener('keyup', () => {
+	//gets the search query from the input field
+	const searchQuery = searchInput.value;
+
+	//construct the API URL with the search query as a parameter
+	const apiUrl = `https://api.rawg.io/api/games?key=a29e01702f3d4aa2a58af885563c92b7&search=${searchQuery}`;
+
+
+	//fetches data from the API, converts the repsonse to JSON and logs it to the console
+	fetch(apiUrl)
+		.then(response => response.json())
+		.then(data => {
+			console.log(data);
+
+		})
+		//logs and errors in the console 
+		.catch(error => console.error(error));
+});
