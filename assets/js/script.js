26740a1a608afdf74dc9f7930e999ecc636e4d28
Frank-Colorado <chawnkiee@gmail.com>
2023-03-27 20:44:06 -0400

    refactored JS code

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,76 +34,73 @@ function fillCards() {
 					cardImgElement.src = backgroundImage;
 
 					i++;
-
-					// const used to make a unique url for cheapshark to run an api
-					const encodedGameTitle = encodeURIComponent(gameTitle);
-					const apiCheapSharkUrl = `https://www.cheapshark.com/api/1.0/games?title=${encodedGameTitle}&exact=0&limit=1`;
-					//console.log(apiCheapSharkUrl);
-
-
-
-					//fetches Cheapshark data via an api call
-					fetch(apiCheapSharkUrl)
-						.then(response => response.json())
-						.then(data => {
-							// loop through each game
-							for (let i = 0; i < data.length; i++) {
-								const game = data[i];
-								const gameId = game.gameID;
-								//console.log(game)
-								//console.log(gameId)
-
-
-								//const used to make a unique url for cheapshark searches its data based on previous gameID
-								const apiCheapSharkDealLookUp = `https://www.cheapshark.com/api/1.0/games?id=${gameId}`;
-								//console.log(apiCheapSharkDealLookUp);
-
-
-								// select all the modal buttons
-								const modalButtons = document.querySelectorAll('.modal-button');
-
-								// iterate through the buttons and attach event listener to each one
-								modalButtons.forEach(button => {
-									button.addEventListener('click', () => {
-										// get the data-mdb-target attribute value, which is the ID of the modal to be displayed
-										const targetModalId = button.getAttribute('data-mdb-target');
-
-										// fetch game prices and links
-										fetch(apiCheapSharkDealLookUp)
-											.then(response => response.json())
-											.then(data => {
-												// update content of modal elements with game prices and links
-												const dealPriceElement = document.querySelector(`${targetModalId} #dealPrice`);
-												dealPriceElement.textContent = data.deals[0].price;
-
-												const retailPriceElement = document.querySelector(`${targetModalId} #retailPrice`);
-												retailPriceElement.textContent = data.deals[0].retailPrice;
-
-												const dealLinkElement = document.querySelector(`${targetModalId} #dealLink`);
-												dealLinkElement.href = `https://www.cheapshark.com/redirect?dealID=${data.deals[0].dealID}`;
-
-												// select the modal element and display it
-												const modal = document.querySelector(targetModalId);
-												modal.style.display = 'block';
-											})
-											.catch(error => {
-												console.error(error);
-											});
-									})
-								})
-							}
-						})
 				}
 			}
+
+
+			for (let k = 0; k < gameTitles.length; k++) {
+				// const used to make a unique url for cheapshark to run an api
+				const encodedGameTitle = encodeURIComponent(gameTitles[k]);
+				const apiCheapSharkUrl = `https://www.cheapshark.com/api/1.0/games?title=${encodedGameTitle}&exact=0&limit=1`;
+				console.log(apiCheapSharkUrl);
+
+
+
+				//fetches Cheapshark data via an api call
+				fetch(apiCheapSharkUrl)
+					.then(response => response.json())
+					.then(data => {
+						// loop through each game
+						for (let j = 0; j < data.length; ++j) {
+							const game = data[j];
+							const gameId = game.gameID;
+
+							//const used to make a unique url for cheapshark searches its data based on previous gameID
+							const apiCheapSharkDealLookUp = `https://www.cheapshark.com/api/1.0/games?id=${gameId}`;
+							//console.log(apiCheapSharkDealLookUp);
+
+							let button_title = ""
+							if (k == 0) {
+								button_title = "modalbutton"
+							} else {
+								button_title = `button_${k}`;
+							}
+							const modalButton = document.getElementById(button_title);
+							
+							console.log(modalButton)
+
+							modalButton.addEventListener('click', () => {
+								// get the data-mdb-target attribute value, which is the ID of the modal to be displayed
+								const targetModalId = modalButton.getAttribute('data-mdb-target');
+
+								// fetch game prices and links
+								fetch(apiCheapSharkDealLookUp)
+									.then(response => response.json())
+									.then(data => {
+										// update content of modal elements with game prices and links
+										const dealPriceElement = document.querySelector(`${targetModalId} #dealPrice`);
+										dealPriceElement.textContent = data.deals[0].price;
+
+										const retailPriceElement = document.querySelector(`${targetModalId} #retailPrice`);
+										retailPriceElement.textContent = data.deals[0].retailPrice;
+
+										const dealLinkElement = document.querySelector(`${targetModalId} #dealLink`);
+										dealLinkElement.href = `https://www.cheapshark.com/redirect?dealID=${data.deals[0].dealID}`;
+
+										// select the modal element and display it
+										const modal = document.querySelector(targetModalId);
+										modal.style.display = 'block';
+									})
+									.catch(error => {
+										console.error(error);
+									});
+							})
+						}
+					})
+			}			
 		})
 
 }
-
-
-
-
-
-
 
 
 fillCards(apiUrl);
