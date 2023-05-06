29f15882c8e90df47115a5e7c48461074aac580a
Frank-Colorado <chawnkiee@gmail.com>
2023-05-05 23:45:30 -0400

    added input eventlistener to 'searchInput'

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,9 @@
 // API VARIABLES
 const apiKey = "a29e01702f3d4aa2a58af885563c92b7";
 const apiUrl = `https://api.rawg.io/api/games?key=${apiKey}&platforms=4`;
+// SEARCH BAR VARIABLES
+const searchInput = document.getElementById("search_input");
+const suggestionsList = document.getElementById("suggestions");
 
 // This is an asynchronous function called 'callRawgApi'
 const callRawgAPI = async (queryParam) => {
@@ -10,10 +13,16 @@ const callRawgAPI = async (queryParam) => {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
     const data = await response.json();
-    console.log(data);
+    return data;
   } catch (error) {
     console.error(error);
   }
 };
+
+// This is an asynchronous function that is called when any input event happens to 'searchInput'
+searchInput.addEventListener("input", async () => {
+  const query = `&search=${searchInput.value}`;
+  const data = await callRawgAPI(query);
+});
 
 callRawgAPI();
