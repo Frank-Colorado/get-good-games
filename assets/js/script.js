2afbac9406c9af88245be41f7137de08419e19c2
Frank-Colorado <chawnkiee@gmail.com>
2023-05-05 23:34:21 -0400

    created a function called 'callRawgApi'

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,19 @@
 // API VARIABLES
 const apiKey = "a29e01702f3d4aa2a58af885563c92b7";
 const apiUrl = `https://api.rawg.io/api/games?key=${apiKey}&platforms=4`;
+
+// This is an asynchronous function called 'callRawgApi'
+const callRawgAPI = async (queryParam) => {
+  try {
+    const response = await fetch(`${apiUrl}${queryParam}`);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+callRawgAPI();
