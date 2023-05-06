babd3b3941097e97d528b5ae3f5dcfcb4737cb71
Frank-Colorado <chawnkiee@gmail.com>
2023-05-06 00:46:10 -0400

    removed onclick function from 'genreTab' elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,7 +13,6 @@ const callRawgAPI = async (queryParam) => {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
     const data = await response.json();
-    console.log(data);
     return data;
   } catch (error) {
     console.error(error);
